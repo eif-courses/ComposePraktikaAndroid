fc301f044441a0848a2e08c5f192474cdc065cdc
Marius Gžegoževskis <m.gzegozevskis@eif.viko.lt>
2021-05-29 01:28:00 +0300

    Last UI Style

--- app/src/main/java/eif/viko/lt/vilniaus/kolegija/elektronikos/myapplication/ui/theme/Type.kt
+++ app/src/main/java/eif/viko/lt/vilniaus/kolegija/elektronikos/myapplication/ui/theme/Type.kt
@@ -1,6 +1,7 @@
 package eif.viko.lt.vilniaus.kolegija.elektronikos.myapplication.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -11,7 +12,7 @@ val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 16.sp,
     )
     /* Other default text styles to override
     button = TextStyle(
